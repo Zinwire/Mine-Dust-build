88df98d7398d9bea5da22a4e6f12be8ac1c2221e
Zinwire <zinwire.dev@gmail.com>
2026-07-09 20:26:30 +0300

    Test uveda

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,3 @@
 Events.on(ClientLoadEvent, () => {
-
-    Vars.ui.hudfrag.showToast(
-        Icon.info,             // Иконка слева
-        "Мод успешно запущен!" // Текст уведомления
-    );
+    Vars.ui.showInfoFade("Мод успешно загружен и готов!");
 });
