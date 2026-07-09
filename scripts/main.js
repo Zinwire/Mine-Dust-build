fae77fe6ae8921d096d8435a78ac99f5746123cc
Zinwire <zinwire.dev@gmail.com>
2026-07-09 19:57:33 +0300

    Добавлено тестовое уведомление

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,9 @@
 
 //КлиентЛоадИвент это при загрузке игры на главном экране
 Events.on(ClientLoadEvent, () => {
-//Создаю впринцепи окно(диалог)
-	const window = new BaseDialog("Тест");
-//Добавляю в "контейнер" окна текст
-	window.cont.add("Test").row();
-	window.cont.add("TEST").padTop(25);
-
-//Добавляю кнопку закрытия окна(Да, она не появляется сама)
-	window.addCloseButton();
-//Показываю окно в игре
-	window.show();
-
+//Мини сообщение показать тут
+	Vars.ui.hudfrag.showToast(
+		Icon.info,
+		"Скрипт на удивление работает"
+	);
 });
