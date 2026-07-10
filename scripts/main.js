3f0e1612696cce49a2c1dc24cbf4263e007f91da
Zinwire <zinwire.dev@gmail.com>
2026-07-10 17:35:21 +0300

    Ubeyte menya

--- scripts/main.js
+++ scripts/main.js
@@ -6,9 +6,11 @@
 //    );
 //});
 
-
+Events.on(ClientLoadEvent, () => {
 const welcomeDialog = new BaseDialog("");
 
 welcomeDialog.titleTable.add("[cyan]Приветствие мода![]").pad(10).row();
 
 welcomeDialog.titleTable.image().color(Pal.accent).height(3).fillX().padTop(5);
+
+});
