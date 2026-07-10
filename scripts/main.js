ebe7402fe798973ea388d77ac9b412893e1a2b39
Zinwire <zinwire.dev@gmail.com>
2026-07-10 18:24:18 +0300

    Играюсь с приветственным окном

--- scripts/main.js
+++ scripts/main.js
@@ -7,12 +7,37 @@
 //});
 
 Events.on(ClientLoadEvent, () => {
-const welcomeDialog = new BaseDialog("");
 
-welcomeDialog.titleTable.add("[cyan]Приветствие мода![]").pad(10).row();
+//Создаю пустой диалог
+    const welcomeDialog = new BaseDialog("");
 
-welcomeDialog.titleTable.image().color(Pal.accent).height(3).fillX().padTop(5);
+//Заголовок
+    welcomeDialog.cont.add("[cyan]Приветствуем вас в моде![]").padTop(10).row();
 
-welcomeDialog.show();
+//Жёлтая полоса через костыль
+    welcomeDialog.cont.image().color(Pal.accent).height(5).width(350).padTop(10).padBottom(15).row();
+
+//Сам текст сообщения
+    welcomeDialog.cont.add("[orange]Я, один из разработчиков мода Mine Dust приветствую вас в моде![]\nМод пока-что находится в бете, по этому могут встречаться блоки/предметы без текстур, и баги :)\nЕсли вам нравится задумка, а может и реализация мода, вы можете поддержать его разработку, \nпоставив звезду моду на [gray]Github[], либо подписавшись на [#24A1DE]телеграм канал[] разработчиков.\n\n[accent]Это сообщение можно выключить в настройках игры[]").pad(10).row();
+
+//Для надёжности настраиваю сообщения заранее
+    welcomeDialog.buttons.clearChildren();
+    welcomeDialog.buttons.defaults().size(210, 50).pad(10);
+
+//Кнопка гитхаба мода
+    welcomeDialog.buttons.button(Icon.github.ui(), "Ссылка на GitHub мода", () => {
+    Core.net.openURI("https://github.com/Zinwire/Mine-Dust")});
+
+//Кнопка закрытия окна
+    welcomeDialog.buttons.button(Icon.ok.ui(), "Хорошо", () => {
+    welcomeDialog.hide();});
+
+//Кнопка перехода в телеграм канал мода
+    welcomeDialog.buttons.button(Icon.link.ui(), "Ссылка на Telegram мода", () => {
+    Core.net.openURI("https://t.me/MineDustMod");});
+
+//Показываю окно
+    welcomeDialog.show();
+
 
 });
