61c743208378d4cdc0d8ad4ee3b9d4ef56a10a68
Zinwire <zinwire.dev@gmail.com>
2026-07-10 17:24:28 +0300

    Пробую вставить коммит из нескольких строк Я думаю выйдет хуйня

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,14 @@
-Events.on(ClientLoadEvent, () => {
-    // Метод принимает: (Заголовок, Основной текст)
-    Vars.ui.showText(
-        "[scarlet]ВАЖНОЕ ОБЪЯВЛЕНИЕ[]", 
-        "Ты долбаёб =)"
-    );
-});
+//Events.on(ClientLoadEvent, () => {
+//    // Метод принимает: (Заголовок, Основной текст)
+//    Vars.ui.showText(
+//        "[scarlet]ВАЖНОЕ ОБЪЯВЛЕНИЕ[]", 
+//        "Ты долбаёб =)"
+//    );
+//});
+
+
+const welcomeDialog = new BaseDialog("");
+
+welcomeDialog.titleTable.add("[cyan]Приветствие мода![]").pad(10).row();
+
+welcomeDialog.titleTable.image().color(Pal.accent).height(3).fillX().padTop(5);
