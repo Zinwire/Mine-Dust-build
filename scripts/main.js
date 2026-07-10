25342c2263a305be606b417085f6e076d3a0f2fa
Zinwire <zinwire.dev@gmail.com>
2026-07-10 21:59:27 +0300

    В очередной раз поменял приветственное сообщение

--- scripts/main.js
+++ scripts/main.js
@@ -29,12 +29,12 @@ Events.on(ClientLoadEvent, () => {
                     () => {
                         // Нажал "GitHub"
                         //Core.net.openURI("https://github.com/Zinwire/Mine-Dust");
-                        Packages.arc.util.OS.openURL("https://github.com/Zinwire/Mine-Dust");
+                        call.openURI("https://github.com/Zinwire/Mine-Dust");
                     },
                     () => {
                         // Нажал "Telegram"
                         //Core.net.openURI("https://t.me/MineDustMod");
-                        Packages.arc.util.OS.openURL("https://t.me/MineDustMod");
+                        call.openURI("https://t.me/MineDustMod");
                     }
                 );
             },
