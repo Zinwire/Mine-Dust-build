ee3742c098a691257797fc26ed4d1830649c1bf3
Zinwire <zinwire.dev@gmail.com>
2026-07-18 23:37:27 +0300

    Обновлены скрипты Изменён баланс в turret.js у турели, добавлены заметки на будущее в /**/, добавлено объяснение кода в комментариях В Main.js добавил рекьюр и объяснил его работу =)

--- scripts/turret.js
+++ scripts/turret.js
@@ -1,7 +1,9 @@
+//Просто для теста сделал вывод в консоль игры текст
 Log.info("Турель считывается");
 
-// 1. Создаем сам блок турели
+//Создаю турель
 const cannon = extend(ItemTurret, "cannon", {
+	//Стандарт параметры как в JSON 
     description: "A big turret with a good blast bullets [W.I.P.]",
     health: 800,
     size: 3,
@@ -10,64 +12,73 @@ const cannon = extend(ItemTurret, "cannon", {
     targetAir: false,
     targetGround: true,
     ammoPerShot: 3,
-    maxAmmo: 10,
+    maxAmmo: 30,
     inaccuracy: 12,
     hasLiquids: true,
     recoil: 2,
     recoilTime: 37,
 
+    //метод при запуске игры в целом (То же самое, что Events.on(ClientLoadEvent, ());)
     init() {
+    	//getByName(ContentType, string) ищет контент по системному названию
         const radiationY = Vars.content.getByName(ContentType.status, "md-Radiation-Y");
         const uranium = Vars.content.getByName(ContentType.item, "md-Uranium");
 
-        // ---- КОД ДЛЯ УГЛЯ ----
+        //Патроны за уголь, мне лень расписывать
         const coalBullet = extend(BasicBulletType, {
-            damage: 45, speed: 4, width: 5, height: 5, lifetime: 32,
+            damage: 45, speed: 3.3, width: 5, height: 5, lifetime: 40,
             homingPower: 0.15, homingRange: 40,
             splashDamage: 45 * 0.4, splashDamageRadius: 10,
             hitEffect: Fx.blastExplosion, despawnEffect: Fx.blastExplosion,
             status: StatusEffects.burning, statusDuration: 5 * 60
+            /* ДОБАВИТЬ ДРУГИЕ ЭФФЕКТЫ */
         });
-        this.ammo(Items.coal, coalBullet);
 
-        // ---- КОД ДЛЯ ВЗРЫВЧАТОЙ СМЕСИ ----
+
+        //Патроны за взрыв смесь
         const bbDamage = 45 - 25;
         const blastBullet = extend(BasicBulletType, {
-            damage: bbDamage, speed: 4, width: 5, height: 5, lifetime: 32,
+            damage: bbDamage, speed: 3.3, width: 5, height: 5, lifetime: 40,
             homingPower: 0.15, homingRange: 40,
             splashDamage: bbDamage * 1.25, splashDamageRadius: 10 + 4,
-            hitEffect: Fx.blastExplosion, despawnEffect: Fx.blastExplosion,
+            hitEffect: Fx.blastExplosion, despawnEffect: Fx.blastExplosion, //Эффекты всякие
             status: StatusEffects.blasted, statusDuration: 5 * 60
+            /* ДОБАВИТЬ ДРУГИЕ ЭФФЕКТЫ */
         });
-        this.ammo(Items.blastCompound, blastBullet);
 
-        // ---- КОД ДЛЯ ТОРИЯ ----
+
+        //Патроны за торий
         const tbDamage = 45 + 40;
         const thoriumBullet = extend(BasicBulletType, {
-            damage: tbDamage, speed: 4, width: 5, height: 5, lifetime: 32,
+            damage: tbDamage, speed: 3.3, width: 5, height: 5, lifetime: 40,
             homingPower: 0.15, homingRange: 40,
             splashDamage: tbDamage * 0.2, splashDamageRadius: 10 - 2,
             hitEffect: Fx.blastExplosion, despawnEffect: Fx.blastExplosion
+            /* ДОБАВИТЬ ДРУГИЕ ЭФФЕКТЫ */
         });
-        if (radiationY != null) {
+        if (radiationY != null) { 				//Проверка для дебага ошибок, чтобы не было вылета
             thoriumBullet.status = radiationY;
             thoriumBullet.statusDuration = 30 * 60;
         }
-        this.ammo(Items.thorium, thoriumBullet);
+
         
-        // ---- КОД ДЛЯ УРАНА ----
+        //Урановые патроны
         if (uranium != null) {
             const ubDamage = 45 + 20;
             const uraniumBullet = extend(BasicBulletType, {
-                damage: ubDamage, speed: 4, width: 5, height: 5, lifetime: 32,
+                damage: ubDamage, speed: 3.3, width: 5, height: 5, lifetime: 40,
                 homingPower: 0.15, homingRange: 40,
                 splashDamage: ubDamage * 0.4, splashDamageRadius: 9,
                 hitEffect: Fx.blastExplosion, despawnEffect: Fx.blastExplosion
+                /* ДОБАВИТЬ ДРУГИЕ ЭФФЕКТЫ */
             });
-            if (radiationY != null) {
+            if (radiationY != null) { 						//Проверка для того же
                 uraniumBullet.status = radiationY;
                 uraniumBullet.statusDuration = 5 * 60 * 60;
             }
+
+
+            //Задаю патроны по типу ("Предмет", "Объект пули")
             this.ammo   (Items.coal, coalBullet,
             			Items.blastCompound, blastBullet,
             			Items.thorium, thoriumBullet,
@@ -75,22 +86,22 @@ const cannon = extend(ItemTurret, "cannon", {
         }
 
         // Настройка очереди выстрелов
-        this.shoot.shots = 3;
-        this.shoot.shotDelay = 5;
+        this.shoot.shots = 3; //Общее кол-во выстрелов
+        this.shoot.shotDelay = 6; //Задержка между выстрелами
 
-        this.super$init();
+        this.super$init(); //Вызов стандартного метода класса
     }
 });
 
-// 2. ИСПРАВЛЕНИЕ ИСЧЕЗНОВЕНИЯ: Принудительно привязываем к турели класс её физического здания на карте.
-// Теперь игра четко поймет, как ставить её в игровом мире, и она БОЛЬШЕ НЕ БУДЕТ ИСЧЕЗАТЬ при постройке!
+// ИСПРАВЛЕНИЕ ИСЧЕЗНОВЕНИЯ: Принудительно привязываю к турели класс её физического здания на карте.
+// Теперь игра четко поймет, как ставить её в игровом мире, и она БОЛЬШЕ НЕ БУДЕТ ИСЧЕЗАТЬ при постройке
 cannon.buildType = () => extend(ItemTurret.ItemTurretBuild, cannon, {});
 
-// 3. Настройка охлаждения
+// Настройка охлаждения (упоротое говнище)
 cannon.consume(new Packages.mindustry.world.consumers.ConsumeCoolant(0.3, true, false)).update = false;
 
-// 4. ИСПРАВЛЕНИЕ ОТОБРАЖЕНИЯ: Заставляем UI-движок игры зарегистрировать турель во всех списках 
-// строго в момент окончания загрузки клиента игры.
+
+// На всякий точно когда всё загрузилось уже назначаю категорию, требования, и принудительный показ блока
 Events.on(ClientLoadEvent, () => {
     const diamond = Vars.content.getByName(ContentType.item, "md-Diamond");
 
