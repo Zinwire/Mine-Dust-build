bd17c87401888617647fec275d1a1e4b58eb2611
Zinwire <zinwire.dev@gmail.com>
2026-07-18 20:28:15 +0300

    На что я свою жизнь трачу?

--- scripts/explosiveTurret.js
+++ scripts/explosiveTurret.js
@@ -1,5 +1,5 @@
 
-//Создание шаблона пули
+
 const exploBulletType = extend(BasicBulletType, {
 
 	damage: 45,
@@ -14,7 +14,6 @@ const exploBulletType = extend(BasicBulletType, {
 	splashDamage: 45 * 0.4,
 	splashDamageRadius: 10,
 
-//Может обновляться позже
 	hitEffect: Fx.blastExplosion,
 	despawnEffect: Fx.blastExplosion
 });
@@ -51,50 +50,106 @@ const cannon = extend(ItemTurret, "Cannon", {
 		const diamond = Vars.content.getByName(ContentType.item, "md-diamond");
 
 
-		const coalBullet = extend(BasicBulletType, Object.assign({}, exploBulletType, {
-            status: StatusEffects.burning,
-            statusDuration: 5 * 60
-        }));
-        this.ammo(Items.coal, coalBullet);
+		const coalBullet = extend(BasicBulletType, {
+			damage: 45,
+			speed: 4,
+			width: 5,
+			height: 5,
+			lifetime: 32,
+
+			homingPower: 0.15,
+			homingRange: 40,
+
+			splashDamage: 45 * 0.4,
+			splashDamageRadius: 10,
+
+			hitEffect: Fx.blastExplosion,
+			despawnEffect: Fx.blastExplosion,
+
+			status: StatusEffects.burning,
+			statusDuration: 5 * 60
+			//Надо будет добавить цвета пули
+		});
+		
+		this.ammo(Items.coal, coalBullet);
 
 
 		const bbDamage = exploBulletType.damage - 25;
-        const blastBullet = extend(BasicBulletType, Object.assign({}, exploBulletType, {
-            damage: bbDamage,
-            splashDamage: bbDamage * 1.25,
-            splashDamageRadius: exploBulletType.splashDamageRadius + 4,
-            status: StatusEffects.blasted,
-            statusDuration: 5 * 60
-        }));
-        this.ammo(Items.blastCompound, blastBullet);
-        
+		const blastBullet = extend(BasicBulletType, {
+			damage: bbDamage,
+			speed: 4,
+			width: 5,
+			height: 5,
+			lifetime: 32,
+
+			homingPower: 0.15,
+			homingRange: 40,
+
+			splashDamage: bbDamage * 1.25,
+			splashDamageRadius: exploBulletType.splashDamageRadius + 4,
+
+			hitEffect: Fx.blastExplosion,
+			despawnEffect: Fx.blastExplosion,
+
+			status: StatusEffects.blasted,
+			statusDuration: 5 * 60
+			//Надо будет добавить цвета пули
+		});
+		this.ammo(Items.blastCompound, blastBullet);
+
+
+
 
 		const tbDamage = exploBulletType.damage + 40;
-        const thoriumBullet = extend(BasicBulletType, Object.assign({}, exploBulletType, {
-            damage: tbDamage,
-            splashDamage: tbDamage * 0.2,
-            splashDamageRadius: exploBulletType.splashDamageRadius - 2
-        }));
-        if (radiationY != null) {
-            thoriumBullet.status = radiationY;
-            thoriumBullet.statusDuration = 30 * 60;
-        }
-        this.ammo(Items.thorium, thoriumBullet);
+		const thoriumBullet = extend(BasicBulletType, {
+			damage: tbDamage,
+			speed: 4,
+			width: 5,
+			height: 5,
+			lifetime: 32,
+
+			homingPower: 0.15,
+			homingRange: 40,
+
+			splashDamage: tbDamage * 0.2,
+			splashDamageRadius: exploBulletType.splashDamageRadius - 2,
+
+			hitEffect: Fx.blastExplosion,
+			despawnEffect: Fx.blastExplosion,
+
+			status: radiationY,
+			statusDuration: 30 * 60
+			//Надо будет добавить цвета пули
+		});
+		this.ammo(Items.thorium, thoriumBullet);
+
+
 		
 
-		if (uranium != null) {
-            const ubDamage = exploBulletType.damage + 20;
-            const uraniumBullet = extend(BasicBulletType, Object.assign({}, exploBulletType, {
-                damage: ubDamage,
-                splashDamage: ubDamage * 0.4,
-                splashDamageRadius: 9
-            }));
-            if (radiationY != null) {
-                uraniumBullet.status = radiationY;
-                uraniumBullet.statusDuration = 5 * 60 * 60;
-            }
-            this.ammo(uranium, uraniumBullet);
-        }
+		if(uranium != null){
+			const ubDamage = exploBulletType.damage + 20;
+			const uraniumBullet = extend(BasicBulletType, {
+			damage: ubDamage,
+			speed: 4,
+			width: 5,
+			height: 5,
+			lifetime: 32,
+
+			homingPower: 0.15,
+			homingRange: 40,
+
+			splashDamage: ubDamage * 0.4,
+			splashDamageRadius: exploBulletType.splashDamageRadius - 1,
+
+			hitEffect: Fx.blastExplosion,
+			despawnEffect: Fx.blastExplosion,
+
+			status: radiationY,
+			statusDuration: 5 * 60 * 60
+			//Надо будет добавить цвета пули
+		});
+			this.ammo(uranium, uraniumBullet);
+		}
 		
 
 
