51633f1c2322cf6f182fa09b19d2b2c614c0efb2
Zinwire <zinwire.dev@gmail.com>
2026-07-25 20:04:35 +0300

    Заменил info на warn для повышения читаемости

--- scripts/libs/logger.js
+++ scripts/libs/logger.js
@@ -5,7 +5,7 @@ function OnBlockError(blockName, exception) {
 	Log.info("[green][md][] [scarlet]Блок @ не был запущен, так как:[]", blockName); //scarlet алый
 	Log.err("[red]@[]", exception.toString()); 								//red более тусклый
 	if(exception.stack){
-		Log.info("[#eaff00]@[]", exception.stack);
+		Log.warn("[#eaff00]@[]", exception.stack);
 	}
 }
 
